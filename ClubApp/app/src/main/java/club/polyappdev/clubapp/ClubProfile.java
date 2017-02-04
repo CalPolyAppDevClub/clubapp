f607856e41a2f31fbfe2303f3c2f68b65335a53c
cchung89 <cchao06@calpoly.edu>
2017-02-04 14:49:12 -0800

    ClubProfile Added
    Add ClubProfile.java for club profile page

---
+++ ClubApp/app/src/main/java/club/polyappdev/clubapp/ClubProfile.java
@@ -0,0 +1,25 @@
+package club.polyappdev.clubapp;
+
+import android.content.Context;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.TextViewCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by chin on 2/4/2017.
+ */
+
+public class ClubProfile extends Fragment {
+
+    
+
+}
