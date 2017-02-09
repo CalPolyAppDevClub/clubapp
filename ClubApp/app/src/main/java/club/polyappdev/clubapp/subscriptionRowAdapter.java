b46a176206c8cb1e6d1d210ef40218f84cc14d95
Daniel-Heyde <dpheyde@gmail.com>
2017-02-09 15:20:59 -0800

    Fixed merge conflicts

--- ClubApp/app/src/main/java/club/polyappdev/clubapp/subscriptionRowAdapter.java
+++ ClubApp/app/src/main/java/club/polyappdev/clubapp/subscriptionRowAdapter.java
@@ -1,15 +1,5 @@
 package club.polyappdev.clubapp;
 
-<<<<<<< HEAD
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.content.Context;
-import java.util.*;
-
-/**
- * Created by Alex on 2/4/17.
-=======
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,20 +12,14 @@ import static android.R.attr.resource;
 
 /**
  * Created by dphey on 2/4/2017.
->>>>>>> 8228bb9d6a37ce1e4c5743ba45341c41dea32cef
  */
 
 public class subscriptionRowAdapter extends BaseAdapter {
 
-<<<<<<< HEAD
-
-    public subscriptionRowAdapter(Context context, int resource) {
-=======
     public subscriptionRowAdapter(Context context, int resource) {
     }
 
     public void setLists(ArrayList<String> inputList, int inputTextView){
->>>>>>> 8228bb9d6a37ce1e4c5743ba45341c41dea32cef
 
     }
 
@@ -44,37 +28,17 @@ public class subscriptionRowAdapter extends BaseAdapter {
         return 0;
     }
 
-<<<<<<< HEAD
-    public void setLists(ArrayList<String> inputList, int inputTextView) {
-
-    }
-
-    @Override
-    public Object getItem(int i) {
-=======
     @Override
     public Object getItem(int position) {
->>>>>>> 8228bb9d6a37ce1e4c5743ba45341c41dea32cef
         return null;
     }
 
     @Override
-<<<<<<< HEAD
-    public long getItemId(int i) {
-=======
     public long getItemId(int position) {
->>>>>>> 8228bb9d6a37ce1e4c5743ba45341c41dea32cef
         return 0;
     }
 
     @Override
-<<<<<<< HEAD
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
-    }
-
-
-=======
     public int getViewTypeCount() {
         return super.getViewTypeCount();
     }
@@ -88,5 +52,4 @@ public class subscriptionRowAdapter extends BaseAdapter {
     public int getItemViewType(int position) {
         return super.getItemViewType(position);
     }
->>>>>>> 8228bb9d6a37ce1e4c5743ba45341c41dea32cef
 }
