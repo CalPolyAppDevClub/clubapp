57676ea9602587ec2b0a5a7f1d67a8af09d87438
Joel Braun <joelbraun@outlook.com>
2017-02-26 01:37:15 -0800

    Fixed broken build.

--- ClubApp/app/src/main/java/club/polyappdev/clubapp/StudentViewable/Profile.java
+++ ClubApp/app/src/main/java/club/polyappdev/clubapp/StudentViewable/Profile.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import club.polyappdev.clubapp.ClubProfileActivity;
 import club.polyappdev.clubapp.R;
 
 
