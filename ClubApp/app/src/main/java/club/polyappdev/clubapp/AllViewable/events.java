069779900df52bbc80c826ad149882c16a358f13
Jacky <huangjacky.1996@gmail.com>
2017-03-11 14:19:17 -0800

    Connecting data from subscribed list to display data on events page for the user

--- ClubApp/app/src/main/java/club/polyappdev/clubapp/AllViewable/events.java
+++ ClubApp/app/src/main/java/club/polyappdev/clubapp/AllViewable/events.java
@@ -1,11 +1,14 @@
 package club.polyappdev.clubapp.AllViewable;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 
 import club.polyappdev.clubapp.R;
@@ -24,6 +27,7 @@ public class events extends AppCompatActivity {
     protected TextView descriptionView;
     protected TextView keywordView;
     protected CheckBox notifymeView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +44,19 @@ public class events extends AppCompatActivity {
         this.descriptionView = (TextView)findViewById(R.id.description);
         this.keywordView = (TextView)findViewById(R.id.keyword);
         this.notifymeView = (CheckBox)findViewById(R.id.notifyBox);
+
+        Intent i = new Intent();
+        i = getIntent();
+        String eventName = i.getStringExtra("eventName");
+        String location = i.getStringExtra("eventStrLoc");
+        String description = i.getStringExtra("eventDesc");
+        String date = i.getStringExtra("eventDate");
+        String clubName = i.getStringExtra("eventClub");
+
+        titleNameView.setText(clubName + ":" +  " " + eventName);
+        locationView.setText("location: " + location);
+        timeView.setText("Date: " + date);
+        descriptionView.setText("Description: " + description);
     }
 
 
