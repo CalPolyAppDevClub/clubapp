4e009c11761f67786168b2b51ac6589c622fc8ba
fykenneth <fykenneth@gmail.com>
2017-04-15 15:43:06 -0700

    Reset password
    Add popup window for reset password

--- ClubApp/app/src/main/java/club/polyappdev/clubapp/MySetting.java
+++ ClubApp/app/src/main/java/club/polyappdev/clubapp/MySetting.java
@@ -1,11 +1,15 @@
 package club.polyappdev.clubapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ScrollView;
@@ -15,6 +19,9 @@ import java.util.List;
 
 import club.polyappdev.clubapp.Models.Club;
 import club.polyappdev.clubapp.Models.Event;
+import club.polyappdev.clubapp.StudentViewable.MainActivity;
+import club.polyappdev.clubapp.StudentViewable.Profile;
+
 /**
  * Created by USER on 3/4/2017.
  */
@@ -25,10 +32,22 @@ public class MySetting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_setting);
-
+        Button cp = (Button) findViewById(R.id.C_P_but);
+        cp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent (MySetting.this, Pop.class));
+            }
+        });
         //Intent intent = getIntent();
         //getSupportActionBar().setTitle("Setting");
 
+    }
+    public void sendMessage(View view)
+    {
+        Log.w("test","test");
+        Intent intent = new Intent(MySetting.this, EditProfile.class);
+        startActivity(intent);
     }
 
 
