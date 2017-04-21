9d3c1f69f90de8b7133ef241aa0ce520010a4805
James Kao <bleach94539@gmail.com>
2017-04-20 17:04:59 -0700

    Added firebase user authentication

--- ClubApp/app/src/main/java/club/polyappdev/clubapp/AllViewable/LoginActivity.java
+++ ClubApp/app/src/main/java/club/polyappdev/clubapp/AllViewable/LoginActivity.java
@@ -1,13 +1,20 @@
 package club.polyappdev.clubapp.AllViewable;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import club.polyappdev.clubapp.ClubViewable.ClubMainActivity;
 import club.polyappdev.clubapp.R;
@@ -16,7 +23,14 @@ import club.polyappdev.clubapp.StudentViewable.MainActivity;
 public class LoginActivity extends AppCompatActivity {
 
     EditText usernameView;
+    EditText passwordView;
     Button loginButton;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +38,24 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
         this.usernameView = (EditText) findViewById(R.id.NameLoginView);
+        this.passwordView = (EditText) findViewById(R.id.PassLoginView);
         this.loginButton = (Button) findViewById(R.id.LoginButton);
         usernameView.requestFocus();
 
@@ -32,12 +63,14 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String username = getUsername();
+                String password = getPassword();
                 if (username.toLowerCase().contains("club")){
                     Intent clubIntent = new Intent(LoginActivity.this, ClubMainActivity.class);
                     startActivity(clubIntent);
                 } else{
-                    Intent userIntent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(userIntent);
+                    signIn(username, password);
+                    /*Intent userIntent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(userIntent);*/
                 }
             }
         });
@@ -46,4 +79,33 @@ public class LoginActivity extends AppCompatActivity {
     private String getUsername(){
         return this.usernameView.getText().toString();
     }
+    private String getPassword() { return this.passwordView.getText().toString(); }
+
+    public void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    public void onStop(){
+        super.onStop();
+        if(mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    public void signIn(String email, String password){
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, R.string.login_error, Toast.LENGTH_SHORT).show();
+                } else{
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
+    }
+
 }
