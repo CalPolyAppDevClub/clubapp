0d3a600cb9da45f27dc84dc5920efca8dd628b47
parrotgeek1 <parrotgeek1@gmail.com>
2017-04-29 16:20:36 -0700

    colors & icons

--- ClubApp/app/src/main/java/club/polyappdev/clubapp/StudentViewable/MainActivity.java
+++ ClubApp/app/src/main/java/club/polyappdev/clubapp/StudentViewable/MainActivity.java
@@ -54,13 +54,12 @@ public class MainActivity extends AppCompatActivity implements
      * The {@link ViewPager} that will host the section contents.
      */
 //    private ViewPager mViewPager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mBottomNavigationView = (BottomNavigationView) findViewById(R.id.BottomNav) ;
+        mBottomNavigationView = (BottomNavigationView) findViewById(R.id.BottomNav);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
 //        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -101,7 +100,7 @@ public class MainActivity extends AppCompatActivity implements
                 .commit();
     }
 
-    private void createFragments(){
+    private void createFragments() {
         Fragment sub = new Subscribed();
         Fragment notif = new Notifications();
         Fragment prof = new Profile();
@@ -111,7 +110,6 @@ public class MainActivity extends AppCompatActivity implements
         mFragmentList.add(general);
         mFragmentList.add(prof);
     }
-
 
 
     @Override
@@ -134,7 +132,7 @@ public class MainActivity extends AppCompatActivity implements
             startActivity(intent2);
         }
         //4.22.2017 modify logout to clear stack and log out of account from firebase - Jacky Huang
-        if (id == R.id.action_logout){
+        if (id == R.id.action_logout) {
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             Toast toast;
             toast = Toast.makeText(this, "You have been \"logged out\"", Toast.LENGTH_SHORT);
@@ -151,86 +149,4 @@ public class MainActivity extends AppCompatActivity implements
     public void onFragmentInteraction(Uri uri) {
 
     }
-//
-//    /**
-//     * A placeholder fragment containing a simple view.
-//     */
-//    public static class PlaceholderFragment extends Fragment {
-//        /**
-//         * The fragment argument representing the section number for this
-//         * fragment.
-//         */
-//        private static final String ARG_SECTION_NUMBER = "section_number";
-//
-//        public PlaceholderFragment() {
-//        }
-//
-//        /**
-//         * Returns a new instance of this fragment for the given section
-//         * number.
-//         */
-//        public static Fragment newInstance(int sectionNumber) {
-//
-//            if (sectionNumber == 1) {
-//                return new Subscribed();
-//            }
-//            else if (sectionNumber == 2) {
-//                return new Notifications();
-//            }
-//            else if (sectionNumber == 3) {
-//                return new General();
-//            }
-//            else if (sectionNumber == 4) {
-//                return new Profile();
-//            }
-//            else {
-//                return new Subscribed();
-//            }
-//        }
-//
-//        @Override
-//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                                 Bundle savedInstanceState) {
-//            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-//            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-//            return rootView;
-//        }
-//    }
-//
-//    /**
-//     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-//     * one of the sections/tabs/pages.
-//     */
-//    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-//
-//        public SectionsPagerAdapter(FragmentManager fm) {
-//            super(fm);
-//        }
-//
-//        @Override
-//        public Fragment getItem(int position) {
-//            // getItem is called to instantiate the fragment for the given page.
-//            // Return a PlaceholderFragment (defined as a static inner class below).
-//            return PlaceholderFragment.newInstance(position + 1);
-//        }
-//
-//        @Override
-//        public int getCount() {
-//            // Show 3 total pages.
-//            return 4;
-//        }
-//
-//        @Override
-//        public CharSequence getPageTitle(int position) {
-//            switch (position) {
-//                case 0:
-//                    return "SECTION 1";
-//                case 1:
-//                    return "SECTION 2";
-//                case 2:
-//                    return "SECTION 3";
-//            }
-//            return null;
-//        }
-//    }
 }
